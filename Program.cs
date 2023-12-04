1c0d4e9b184bc85bfd12b221934e4cb73e6abef9
Vladimir <w112vovan@mail.ru>
2023-12-04 12:27:27 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L34_queueAtStore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int minValue = 10;
+            int maxValue = 30;
+            int wallet = 0;
+
+            Queue<int> buyers = GetQueueBuyers(ref minValue, ref maxValue);
+            ServeClients(ref buyers, ref wallet);
+        }
+
+        static Queue<int> GetQueueBuyers(ref int minValue, ref int maxValue)
+        {
+            Random random = new Random();
+            Queue<int> tempQueue = new Queue<int>();
+
+            for (int i = minValue; i <= maxValue; i++)
+                tempQueue.Enqueue(random.Next(minValue, maxValue + 1));
+
+            return tempQueue;
+        }
+
+        static void ServeClients(ref Queue<int> queue, ref int wallet)
+        {
+            int queueLenght = queue.Count;
+
+            for (int i = 0; i < queueLenght; i++)
+            {
+                wallet += queue.Dequeue();
+                Console.WriteLine($"Клиент обслужен, текущая сумма на счету: {wallet} деревянных.");
+                Console.WriteLine($"Клиентов в очереди осталось: {queue.Count}.\n");
+
+                Console.WriteLine("Для продолжения обслуживания клиентов, нажмите любую клавишу...");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            Console.WriteLine($"Все клиенты обслуженны.\nИтоговая сумма на счету: {wallet} деревянных.\n");
+        }
+    }
+}
